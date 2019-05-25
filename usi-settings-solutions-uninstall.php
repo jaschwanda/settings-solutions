610a35f7c1d6ef679d989dba54945e7805830851
jaschwanda <iJAS@usi2solve.com>
2019-05-24 22:10:12 -0400

    Added uninstall class to delete settings options.

--- usi-settings-solutions-uninstall.php
+++ usi-settings-solutions-uninstall.php
@@ -1,34 +1,17 @@
 <?php // ------------------------------------------------------------------------------------------------------------------------ //
 
-if (!function_exists('usi_log')) {
-   function usi_log($action) {
-      global $wpdb;
-      $wpdb->insert($wpdb->prefix . 'USI_log', 
-         array(
-            'action' => $action,
-            'user_id' => get_current_user_id(), 
-         )
-      );
-   } // usi_log();
-} // ENDIF function_exists('usi_log');
-
 final class USI_Settings_Solutions_Uninstall {
 
-   const VERSION = '1.1.0 (2019-05-14)';
+   const VERSION = '2.0.0 (2019-04-13)';
 
    private function __construct() {
    } // __construct();
 
    static function uninstall($prefix) {
-usi_log(__METHOD__.':'.__LINE__.':prefix=' . $prefix);
 
       if (!defined('WP_UNINSTALL_PLUGIN')) exit;
 
-      global $wpdb;
-
-//      $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}USI_variables");
-
-//      delete_metadata('user', null, $wpdb->prefix . USI_Variable_Solutions::PREFIX . '-options-category', null, true);
+      delete_option($prefix . '-options');
 
    } // uninstall();
 
