8c3264bd5e63d1ff970fd4640045546b3919b365
jaschwanda <iJAS@usi2solve.com>
2019-05-29 18:37:05 -0400

    Updated uninstall functionality.

--- usi-settings-solutions-uninstall.php
+++ usi-settings-solutions-uninstall.php
@@ -12,8 +12,6 @@ if (!function_exists('usi_log')) {
    } // usi_log();
 } // ENDIF function_exists('usi_log');
 
-usi_log(__FILE__);
-
 final class USI_Settings_Solutions_Uninstall {
 
    const VERSION = '2.0.0 (2019-04-13)';
@@ -24,12 +22,20 @@ final class USI_Settings_Solutions_Uninstall {
    static function uninstall($prefix) {
 
       if (!defined('WP_UNINSTALL_PLUGIN')) exit;
-      $status = delete_option($prefix . '-options');
-usi_log(__METHOD__.':'.__LINE__.':delete_option=' . ($status ? 'yes' : 'no') . '(' . $prefix . '-options)');
-      $status = delete_metadata('user', null, $wpdb->prefix . $prefix . '-options%', null, true);
-usi_log(__METHOD__.':'.__LINE__.':delete_metadata=' . ($status ? 'yes' : 'no') . '(' . $wpdb->prefix . $prefix . '-options%)');
-      //update_user_option($current_user_id, $prefix . '-options-role-id', $this->role_id);
-      //update_user_option($current_user_id, $prefix . '-options-user-id', $this->user_id);
+
+      global $wpdb;
+      $results = $wpdb->get_results('SELECT option_name FROM ' . $wpdb->prefix . 'options WHERE (option_name LIKE "' . $prefix . '-options%")');
+      foreach ($results as $result) {
+         $status = delete_option($result->option_name);
+usi_log(__METHOD__.':'.__LINE__.':delete_option=' . ($status ? 'yes' : 'no') . '(' . $result->option_name . ')');
+      }
+
+      $results = $wpdb->get_results('SELECT DISTINCT meta_key FROM ' . $wpdb->prefix . 'usermeta WHERE (meta_key LIKE "' . $wpdb->prefix . $prefix . '-options%")');
+usi_log(__METHOD__.':'.__LINE__.':SELECT DISTINCT meta_key FROM ' . $wpdb->prefix . 'usermeta WHERE (meta_key LIKE "' . $wpdb->prefix . $prefix . '-options%")');
+      foreach ($results as $result) {
+         $status = delete_metadata('user', null, $result->meta_key, null, true);
+usi_log(__METHOD__.':'.__LINE__.':delete_metadata=' . ($status ? 'yes' : 'no') . '(' . $result->meta_key . ')');
+      }
 
    } // uninstall();
 
