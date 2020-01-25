a046028286f6a3ab5aac946fb3cfd01947aec5f1
jaschwanda <iJAS@usi2solve.com>
2020-01-25 10:38:25 -0500

    Improved ability to string multiple fields on the same line by enabling a conditional over ride of WordPress settings API functions.

--- usi-wordpress-solutions.php
+++ usi-wordpress-solutions.php
@@ -15,7 +15,7 @@ Requires at least: 5.0
 Requires PHP:      5.6.25
 Tested up to:      5.3.2
 Text Domain:       usi-wordpress-solutions
-Version:           2.3.4
+Version:           2.3.5
 */
 
 /*
@@ -33,7 +33,7 @@ Copyright (c) 2020 by Jim Schwanda.
 
 final class USI_WordPress_Solutions {
 
-   const VERSION = '2.3.4 (2020-01-24)';
+   const VERSION = '2.3.5 (2020-01-25)';
 
    const NAME       = 'WordPress-Solutions';
    const PREFIX     = 'usi-wordpress';
